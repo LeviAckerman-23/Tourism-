63abe67b75e97b8d066ac4da03f29d6f9a72c33e
Itz Levi.. <148079885+LeviAckerman-23@users.noreply.github.com>
2024-08-18 18:07:39 +0530

    Add files via upload

---
+++ EmbedMap.php
@@ -0,0 +1,11 @@
+<?php
+echo '<iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15702.48887226395!2d76.50424050442399!3d10.29199203725993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ff02bf6d87a5%3A0x5cffa858845b2c6e!2sAthirappilly%2C%20Vettilappara%2C%20Pariyaram%2C%20Kerala%20680724!5e0!3m2!1sen!2sin!4v1723983612307!5m2!1sen!2sin"
+    width="100%"
+    height="100%"
+    style="border:0;"
+    allowfullscreen=""
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade">
+</iframe>';
+?>
